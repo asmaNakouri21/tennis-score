8f50dc24346500dfcd6b2d98168bd249ee54a92f
AN0B531L <asma-externe.nakouri@enedis.fr>
2024-12-02 10:53:03 +0100

    Refactor and add more tests for deuce scenario

--- src/test/java/com/example/tennis/TennisGameTest.java
+++ src/test/java/com/example/tennis/TennisGameTest.java
@@ -2,13 +2,15 @@ package com.example.tennis;
 
 import com.example.tennis.exception.UnknownPlayerException;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TennisGameTest {
     @Test
-    public void should_write_scenario_for_player_A_Won_all_balls() throws UnknownPlayerException {
+    @DisplayName("Should correctly return the score details when Player A wins all the balls in a game")
+    public void shouldReturnCorrectScoreForPlayerAWinAllBals() throws UnknownPlayerException {
 
         // GIVEN
         String inputScore = "AAAA";
@@ -27,7 +29,8 @@ public class TennisGameTest {
     }
 
     @Test
-    public void should_write_scenario_for_player_B_Won_all_balls() throws UnknownPlayerException {
+    @DisplayName("Should correctly return the score details when Player B wins all the balls in a game")
+    public void shouldReturnCorrectScoreForPlayerBWinAllBals() throws UnknownPlayerException {
 
         // GIVEN
         String inputScore = "BBBB";
@@ -45,9 +48,20 @@ public class TennisGameTest {
         assertEquals(expectedScore, actualScenario);
     }
 
+    @Test
+    @DisplayName("Should throw an error when inputScore gets other player then A and B")
+    public void shouldGetErrorWhenScorerListIsInvalid() {
+        // GIVEN
+        String inputScore = "ACAZ";
+        // WHEN
+        TennisGame tennisGame = new TennisGame();
+        // THEN
+        assertThrows(UnknownPlayerException.class , () -> tennisGame.getScoreDetails(inputScore));
+    }
 
     @Test
-    public void should_get_error_when_scorer_list_is_invalid_cas1() {
+    @DisplayName("Should throw an error when Player A scores 5 points and Player B scores no points.")
+    public void shouldGetErrorWhenScorerListIsInvalidCas2() {
         // GIVEN
         String inputScore = "AAAAA";
 
@@ -59,17 +73,8 @@ public class TennisGameTest {
     }
 
     @Test
-    public void should_get_error_when_scorer_list_is_invalid() {
-        // GIVEN
-        String scorerList = "ACAZ";
-        // WHEN
-        TennisGame tennisGame = new TennisGame();
-        // THEN
-        assertThrows(UnknownPlayerException.class , () -> tennisGame.getScoreDetails(scorerList));
-    }
-
-    @Test
-    public void should_print_scenario_when_player_B_win_after_remontada_before_deuce() throws UnknownPlayerException {
+    @DisplayName("Should correctly return the score details when Player A wins 2 points and Player B wins 3 points and the game")
+    public void shouldReturnCorrectScoreDetailsWhenPlayerBWinsGame() throws UnknownPlayerException {
         // GIVEN
         String inputScore = "AABBBB";
         var expectedScore = List.of(
@@ -86,8 +91,10 @@ public class TennisGameTest {
         assertEquals(expectedScore, actualScenario);
     }
     @Test
-    public void should_print_scenario_when_player_B_win_in_random_scenario() throws UnknownPlayerException {
+    @DisplayName("Should correctly return the score details when Player B wins 2 points and Player A wins 4 points and the game")
+    public void shouldReturnCorrectScoreDetailsWhenPlayerAWinsGame() throws UnknownPlayerException {
         // GIVEN
+        String inputScore = "ABABAA";
         var expectedScore = List.of(
                 "Player A : 15 / Player B : 0", // A score
                 "Player A : 15 / Player B : 15", // B score
@@ -97,14 +104,17 @@ public class TennisGameTest {
                 "Player A wins the game");       // A score
         // WHEN
         TennisGame tennisGame = new TennisGame();
-        var actualScenario = tennisGame.getScoreDetails("ABABAA");
+        var actualScenario = tennisGame.getScoreDetails(inputScore);
         // THEN
         assertEquals(expectedScore, actualScenario);
     }
 
     @Test
-    public void should_print_scenario_when_player_A_win_after_deuce_and_one_advantage() throws UnknownPlayerException {
+    @DisplayName("Should correctly return score details when the game reaches deuce after each player wins 4 points, and then Player A wins")
+    public void shouldReturnCorrectScoreDetailsWhenGameReachesDeuceAndPlayerAWins() throws UnknownPlayerException {
         // GIVEN
+        String inputScore = "ABABBAAA";
+
         var expectedScenario = List.of(
                 "Player A : 15 / Player B : 0",
                 "Player A : 15 / Player B : 15",
@@ -116,9 +126,36 @@ public class TennisGameTest {
                 "Player A wins the game");
         // WHEN
         TennisGame tennisGame = new TennisGame();
-        var actualScenario = tennisGame.getScoreDetails("ABABBAAA");
+        var actualScenario = tennisGame.getScoreDetails(inputScore);
         // THEN
         assertEquals(expectedScenario, actualScenario);
+    }
+
+    @Test
+
+    @DisplayName("Should correctly return score details when game reaches deuce twice and Player B wins")
+    public void shouldReturnCorrectScoreDetailsWhenGameReachesDeuceAndPlayerBWins() throws UnknownPlayerException {
+        // GIVEN
+        String inputScore = "BAABABABBB";
+        var expectedScenario = List.of(
+                "Player A : 0 / Player B : 15",  // B score
+                "Player A : 15 / Player B : 15", // A score
+                "Player A : 30 / Player B : 15", // A score
+                "Player A : 30 / Player B : 30", // B score
+                "Player A : 40 / Player B : 30", // A score
+                "Deuce", // B score : deuce
+                "Deuce, advantage for Player A", // A score : deuce with A advantaged
+                "Deuce", // B score : deuce with B advantaged
+                "Deuce, advantage for Player B", // B score : deuce with A advantaged
+                "Player B wins the game");       // B score and wins
+
+        // WHEN
+        TennisGame tennisGame = new TennisGame();
+        var actualScenario = tennisGame.getScoreDetails(inputScore);
+
+        // THEN
+        assertEquals(expectedScenario, actualScenario);
+
     }
 
 }
