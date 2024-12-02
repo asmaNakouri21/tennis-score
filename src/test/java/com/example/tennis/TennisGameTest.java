d396900d8151e68ae28a1fe38eb628940a54a9a9
AN0B531L <asma-externe.nakouri@enedis.fr>
2024-12-02 05:28:27 +0100

    Implement test for deuce scenerio

--- src/test/java/com/example/tennis/TennisGameTest.java
+++ src/test/java/com/example/tennis/TennisGameTest.java
@@ -68,4 +68,57 @@ public class TennisGameTest {
         assertThrows(UnknownPlayerException.class , () -> tennisGame.getScoreDetails(scorerList));
     }
 
+    @Test
+    public void should_print_scenario_when_player_B_win_after_remontada_before_deuce() throws UnknownPlayerException {
+        // GIVEN
+        String inputScore = "AABBBB";
+        var expectedScore = List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 30 / Player B : 0",
+                "Player A : 30 / Player B : 15",
+                "Player A : 30 / Player B : 30",
+                "Player A : 30 / Player B : 40",
+                "Player B wins the game");
+        // WHEN
+        TennisGame tennisGame = new TennisGame();
+        var actualScenario = tennisGame.getScoreDetails(inputScore);
+        // THEN
+        assertEquals(expectedScore, actualScenario);
+    }
+    @Test
+    public void should_print_scenario_when_player_B_win_in_random_scenario() throws UnknownPlayerException {
+        // GIVEN
+        var expectedScore = List.of(
+                "Player A : 15 / Player B : 0", // A score
+                "Player A : 15 / Player B : 15", // B score
+                "Player A : 30 / Player B : 15", // A score
+                "Player A : 30 / Player B : 30", // B score
+                "Player A : 40 / Player B : 30", // A score
+                "Player A wins the game");       // A score
+        // WHEN
+        TennisGame tennisGame = new TennisGame();
+        var actualScenario = tennisGame.getScoreDetails("ABABAA");
+        // THEN
+        assertEquals(expectedScore, actualScenario);
+    }
+
+    @Test
+    void should_print_scenario_when_player_A_win_after_deuce_and_one_advantage() throws UnknownPlayerException {
+        // GIVEN
+        var expectedScenario = List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 15 / Player B : 15",
+                "Player A : 30 / Player B : 15",
+                "Player A : 30 / Player B : 30",
+                "Player A : 30 / Player B : 40",
+                "Deuce",
+                "Deuce, advantage for Player A",
+                "Player A wins the game");
+        // WHEN
+        TennisGame tennisGame = new TennisGame();
+        var actualScenario = tennisGame.getScoreDetails("ABABBAAA");
+        // THEN
+        assertEquals(expectedScenario, actualScenario);
+    }
+
 }
